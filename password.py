044f24260d7a42e2bee117fce90d4a66ac42313c
Chen Jui <larstraschen@gmail.com>
2020-03-28 17:09:10 +0800

    帳號鎖住

--- password.py
+++ password.py
@@ -1,16 +1,16 @@
-x = 4
+password = 'a123456'
+x = 3
 while True:
-	password = input('請輸入密碼: ')
-	x = x -1
-	if password == 'a1234':
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
 		print('登入成功')
 		break
-	elif x == 0:
-		print('超過次數禁止登入')
-		break
-	elif password != 'a1234':
-		print('密碼錯誤!還有')
-		print(x)
-		print('機會')
-	
-	
+	else:
+		x = x - 1
+		print('密碼錯誤')
+		if x > 0:
+			print('還有',x,'機會')
+		else:
+			print('超過使用次數，帳後被鎖')
+			break
+
